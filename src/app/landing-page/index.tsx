c910bf6a501d9e37075f9ad2c193e86073b8a30a
Sirine Soltani <104676312+sirine200@users.noreply.github.com>
2025-02-20 16:40:52 +0100

    Update Headline

--- src/app/landing-page/index.tsx
+++ src/app/landing-page/index.tsx
@@ -8,10 +8,10 @@ type Props = {}
 const LandingPage = (props: Props) => {
   return (
     <PlausibleProvider domain={process.env.PLAUSIBLE_DOMAIN || ""}>
-      <section className="flex flex-col items-center justify-center space-y-4 pt-4 sm:pt-24 w-full bg-[url('/grid.SVG')]" id="hero">
-        <h1 className='text-4xl font-bold text-center tracking-tighter sm:text-5xl md:text-6xl leading-6'>Create your forms <br></br>in seconds not hours</h1>
+      <section className="flex flex-col items-center justify-center space-y-4 pt-4 sm:pt-24 w-full bg-[url('/images/app/back.png')] bg-cover bg-center bg-opacity-50" id="hero">
+        <h1 className='text-4xl font-bold text-center tracking-tighter sm:text-5xl md:text-6xl leading-6'>Generate Smarter Forms <br></br>Instantly with AI</h1>
         <p className='max-w-[600px] mt-4 text-center text-gray-500 md:textl-xl'>
-          Generate, publish and share your form right away with AI. Dive into insightful results, charts and analytics.
+          Effortlessly generate, customize, and share your forms with AI. No hassle, just results.
         </p>
         <FormGenerator />
         <div className='w-full bg-gradient-to-b from-transparent to-white h-24'></div>
